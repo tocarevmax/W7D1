6029cd9cc7b525f77192b6fb8af4051abcc2023b
Maxim Tocarev <max.tocarev@gmail.com>
2017-11-13 13:39:09 -0800

    Initial commit

---
+++ frontend/reducers/todos_reducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+  1: {
+    id: 1,
+    title: 'wash car',
+    body: 'with soap',
+    done: false
+  },
+  2: {
+    id: 2,
+    title: 'wash dog',
+    body: 'with shampoo',
+    done: true
+  },
+};
+
+const todosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    // case expression:
+    default: 
+      return state;
+  }
+};
